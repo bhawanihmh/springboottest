a39f69e64f1469aa2ff7ecbf940920b363f40a4e
BHAWANI SINGH SHEKHAWAT <bhawani.hmh@gmail.com>
2017-04-28 13:14:49 +0530

    Update SpringboottestApplication.java

--- src/main/java/com/example/SpringboottestApplication.java
+++ src/main/java/com/example/SpringboottestApplication.java
@@ -1,12 +1,26 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class SpringboottestApplication {
+public class SpringboottestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboottestApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringboottestApplication.class);
+    }
 }
+
