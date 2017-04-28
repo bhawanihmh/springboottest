e159fbcdfa3a5d8c5e29efd60318a6feadcb09e3
BHAWANI SINGH SHEKHAWAT <bhawani.hmh@gmail.com>
2017-04-28 13:58:19 +0530

    Update HpgdtvRestController.java

--- src/main/java/com/example/HpgdtvRestController.java
+++ src/main/java/com/example/HpgdtvRestController.java
@@ -1,7 +1,5 @@
 package com.example;
 
-//import javax.enterprise.inject.Any;
-//import javax.inject.Inject;
 
 import java.util.logging.Logger;
 
@@ -9,58 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
- * @author bhawani
+ * @author bhawani.singh
  */
-// @Path("/hpgdtv")
-
 @RestController
-@RequestMapping("/hpgdtv")
-public class HpgdtvRestController {
-
-	/*
-	 * @Inject
-	 * 
-	 * @Any private LoggingFraction fraction;
-	 */
-
-	HpgdtvRestController() {
-		System.out.println("@@@@@@@@@@@@@@@@@   HpgDtvController.HpgDtvController()");
-	}
-
+public class HelloRestController {
+	
 	@SuppressWarnings("unused")
-	private static final Logger LOG = Logger.getLogger(HpgdtvRestController.class.getName());
-	/*
-	 * @GET
-	 * 
-	 * @Consumes({"application/xml", "application/json"}) public Response
-	 * getDtvList() { System.out.println( "Hello World!" ); String str =
-	 * "Hello World!"; //JsonObject jsonObject = Json.createReader(new
-	 * StringReader(str)).readObject(); return Response.ok(str).build(); }
-	 */
-
-	@RequestMapping(value = "/hpgdtvChannels", method = RequestMethod.GET)
-	@ResponseBody
-	public String getHpgdtvChannels() {
-		// log
-		LOG.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   getHpgdtvChannels");
-		return " getHpgdtvChannels Hello Bhawani required dtvChannels ????";
-	}
-
-	@RequestMapping(value = "/satendar", method = RequestMethod.GET)
-	@ResponseBody
-	public String getList() {
-		// log
-		LOG.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   getList");
-
-		return " getList Hello Bhawani required dtvChannels ????";
-	}
+	private static final Logger LOG = Logger.getLogger(HelloRestController.class.getName());
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public ModelAndView hello() {
-		return new ModelAndView("hello").addObject("name", "Bhawani");
-	}
+	@ResponseBody
+    public String hello(){
+        return ("Hello...... Spring Boot delpoyed on Wildfly");
+    }
 
 }
